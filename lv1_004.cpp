f982b97ac62c3afa9ec19f78f50ec132e182b679
LEE MYEONG JIN <71704247+NoirNorie@users.noreply.github.com>
2022-02-19 02:38:30 +0900

    Create lv1_004.cpp
    string도 반복자를 사용할 수 있다는 사실을 자꾸 잊는다.

---
+++ lv1_004.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string solution(string new_id) {
+	string tmp = "";
+
+	for (size_t i = 0; i < new_id.length(); i++)
+	{
+		if (isalnum(new_id[i]))
+		{
+			tmp += tolower(new_id[i]);
+		}
+		else if (new_id[i] == '-' || new_id[i] == '_')
+		{
+			tmp += new_id[i];
+		}
+		else if (new_id[i] == '.')
+		{
+			if (tmp.length() == 0) // .이 가장 먼저 나온다면 스킵
+			{
+				continue;
+			}
+			else if (tmp[tmp.length() - 1] == '.') // .이 연속해서 나온다면 스킵
+			{
+				continue;
+			}
+			else
+			{
+				tmp += new_id[i];
+			}
+		}
+	}
+
+    if(tmp.empty()) tmp += "a";
+    if(tmp.front() == '.') tmp.erase(tmp.begin());
+    if(tmp.back() == '.') tmp.pop_back();
+    if(tmp.length() >= 16) tmp = tmp.substr(0, 15);
+    if(tmp.back() == '.') tmp.pop_back();
+    while(tmp.length() <= 2) tmp += tmp.back();
+    
+	return tmp;
+}
