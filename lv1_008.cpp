7c8012f07bec4bfaea44bd61603af9ca89bf1586
LEE MYEONG JIN <71704247+NoirNorie@users.noreply.github.com>
2022-02-20 15:41:13 +0900

    Create lv1_008.cpp

---
+++ lv1_008.cpp
@@ -0,0 +1,39 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int solution(vector<int> nums) {
+    int answer = 0;
+    vector<int> tmp(nums.size(), 1);
+    for(size_t i = 0 ; i < nums.size() - 3; i++)
+    {
+        tmp[i] = 0;
+    }
+    
+    do
+    {
+        int p = 0; bool chk = false;
+        for (size_t i = 0; i < tmp.size(); i++)
+        {
+            if (tmp[i] == 1)
+            {
+                p += nums[i];
+            }
+        }
+        for (int i = 2; i * i <= p; i++)
+        {
+            if (p % i == 0)
+            {
+                chk = true;
+            }
+        }
+        if (chk == false)
+        {
+            answer++;
+        }        
+    }while(next_permutation(tmp.begin(), tmp.end()));
+
+    return answer;
+}
