653be93f7de1d92ac05122e00d2170eabdf18021
LEE MYEONG JIN <71704247+NoirNorie@users.noreply.github.com>
2022-02-21 03:01:53 +0900

    Create lv1_010.cpp

---
+++ lv1_010.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <algorithm>
+#include <set>
+using namespace std;
+
+int solution(vector<int> nums)
+{
+	int answer = 0;
+	set<int> currentSelect(nums.begin(), nums.end());
+    if(currentSelect.size() > nums.size() / 2) answer = nums.size()/2;
+    else answer = currentSelect.size();
+	
+    return answer;
+}
+
+// 사실 더 짧게도 된다.
+
+int solution(vector<int> nums)
+{
+	set<int> currentSelect(nums.begin(), nums.end());
+  return min(currentSelect.size(), nums.size()/2);
+}
