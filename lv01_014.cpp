0723fba7d409642e2db96b5b69542f3d8593dda6
LEE MYEONG JIN <71704247+NoirNorie@users.noreply.github.com>
2022-02-24 18:44:41 +0900

    Create lv01_014.cpp

---
+++ lv01_014.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int a, int b) {
+    vector<string> day = {"FRI", "SAT", "SUN", "MON", "TUE", "WED","THU"};
+    vector<int> month = { 31,29,31,30,31,30,31,31,30,31,30,31}; // 잊고 있었는데 7월과 8월은 둘 다 31일이다.
+    int daydif = b-1;
+    for(int i = 1 ; i < a; i++)
+    {
+        daydif += month[i-1];
+    }
+    string answer = day[daydif % 7];
+    return answer;
+}
